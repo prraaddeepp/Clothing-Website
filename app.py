acfc5b9a6097d06d2e2c243a006758a18f44c155
Sanjaya-subedi <sanjayasubedi@techexchange.in>
2022-04-16 20:15:46 +0000

    Organizing files and folders

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, session, url_for
+from flask_pymongo import PyMongo
+
+import secrets
+
+
+# # -- Initialization section --
+app = Flask(__name__)
+
+# # name of database
+app.config['MONGO_DBNAME'] = 'database'
+
+# # URI of database
+# # Accessed from CONFIG VARS
+secret_key = os.environ.get('MONGO_URI')
+app.config['MONGO_URI'] = "mongodb+srv://admin:iamCool100!@cluster0.dhqv0.mongodb.net/Final_Project?retryWrites=true&w=majority"
+
+# #Initialize PyMongo
+mongo = PyMongo(app)
+app.secret_key = secrets.token_urlsafe(16)
+
+@app.route('/')
+#INDEX Route
+@app.route('/index')
+def index():
+    return render_template('index.html')
