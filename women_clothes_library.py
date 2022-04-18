4ff09676692c1894d92fcc5286a600570292a3a0
Sanjaya-subedi <sanjayasubedi@techexchange.in>
2022-04-18 02:03:47 +0000

    Changes to men and women file

---
+++ women_clothes_library.py
@@ -0,0 +1,16 @@
+women_clothes = [
+    {"name":"Dress","description":"Blush Floral Print High Low Dress", "size":"M", "Price":25.00, "Source":"./images/women/1.jpg"},
+    {"name":"Blouse","description":"Nose Neck Solid Blouse", "size":"S", "Price":15.00, "Source":"./images/women/2.jpg"},
+    {"name":"Shirt","description":"Women's Scotch Plaid Flannel Shirt", "size":"M", "Price":20.00, "Source":"./images/women/3.jpg"},
+    {"name":"Blouse","description":"Half Sleeve Floral V Neck Shift Blouse", "size":"S", "Price":15.00, "Source":"./images/women/4.jpg"},
+    {"name":"Suit","description":"Pencil Skirt Suit Set", "size":"L", "Price":30.00, "Source":"./images/women/5.jpg"},
+    {"name":"Robe","description":"Women's Organic Terry Robe", "size":"L", "Price":20.00, "Source":"./images/women/6.jpg"},
+    {"name":"Jacket","description":"Women's Classic Utility Jacket", "size":"S", "Price":30.00, "Source":"./images/women/7.jpg"},
+    {"name":"Shorts","description":"Women's Wrinkle Free Bayside Shorts", "size":"M", "Price":25.00, "Source":"./images/women/8.jpg"},
+    {"name":"Heel","description":"Caradise Women's Low Heel Chunky Mary", "size":"M", "Price":35.00, "Source":"./images/women/9.jpg"},
+    {"name":"Boot","description":"High Heel Boot", "size":"S", "Price":25.00, "Source":"./images/women/10.jpg"},
+    {"name":"Hat","description":"Butterfly Hat", "size":"M", "Price":15.00, "Source":"./images/women/11.jpg"},
+    {"name":"Chino","description":"Women's Chino Pant", "size":"M", "Price":35.00, "Source":"./images/women/12.jpg"},
+
+    
+]
